cc493a5783e3f9dee9957ad1e8fd2d98c5057680
Bogdan Solga <bogdan.solga@gmail.com>
2018-11-18 07:11:16 +0200

    [dev] Added an event streaming example

--- src/main/java/net/safedata/reactive/spring/controller/ProductController.java
+++ src/main/java/net/safedata/reactive/spring/controller/ProductController.java
@@ -4,11 +4,15 @@ import net.safedata.reactive.spring.domain.entity.Product;
 import net.safedata.reactive.spring.domain.repository.ProductRepository;
 import org.reactivestreams.Publisher;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.Instant;
 
 @RestController
 @RequestMapping("/product")
@@ -35,5 +39,14 @@ public class ProductController {
     public Publisher<Product> many() {
         return Flux.<Product>generate(sink -> sink.next(new Product(5, "iSome", 200)))
                 .take(50);
+    }
+
+    @GetMapping(
+            value = "/stream",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE
+    )
+    public Publisher<Product> infiniteStreamOfProducts() {
+        return Flux.<Product>generate(sink -> sink.next(new Product(10, "The product for " + Instant.now(), 200)))
+                   .delayElements(Duration.ofSeconds(1));
     }
 }

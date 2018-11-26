88db7267253e10a32c9a03a07be31a413cd64e0e
Bogdan Solga <bogdan.solga@gmail.com>
2018-11-26 19:23:05 +0200

    [dev] Re-enabled the publishing of Order messages

--- spring-cloud-stream-publisher/src/main/java/net/safedata/reactive/spring/cloud/stream/CloudStreamPublisherApplication.java
+++ spring-cloud-stream-publisher/src/main/java/net/safedata/reactive/spring/cloud/stream/CloudStreamPublisherApplication.java
@@ -8,20 +8,15 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
-import org.springframework.cloud.stream.annotation.Input;
 import org.springframework.cloud.stream.annotation.Output;
-import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Source;
 import org.springframework.context.annotation.Bean;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.MessageHeaders;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.util.MimeTypeUtils;
-import reactor.core.publisher.Flux;
 
-import java.time.Duration;
 import java.time.LocalDateTime;
 
 @SpringBootApplication
@@ -42,11 +37,11 @@ public class CloudStreamPublisherApplication {
 		};
 	}
 
-	private void sendOrderAsMessage(final Source source, final int orderId) {
-		final Product product = new Product(orderId, "Tablet " + orderId, 200 * orderId);
-		final Message<String> orderMessage = MessageBuilder.withPayload(new Order(1, product, LocalDateTime.now()).toString())
-														  //.setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
-														  .build();
+	private void sendOrderAsMessage(final Source source, final int number) {
+		final Product product = new Product(number, "Tablet " + number, 200 * number);
+		final Message<Order> orderMessage = MessageBuilder.withPayload(new Order(number, product, LocalDateTime.now()))
+														   .setHeader(MessageHeaders.CONTENT_TYPE, MimeTypeUtils.APPLICATION_JSON)
+														   .build();
 		LOGGER.info("Sending a message for the order '{}'...", orderMessage.getPayload());
 		source.output().send(orderMessage);
 	}

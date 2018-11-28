4679c288b0be7d2c7a14003d0542b42f59d04425
Bogdan Solga <bogdan.solga@gmail.com>
2018-11-28 09:24:50 +0200

    [improve] Replaced the System.out with a logger

--- spring-cloud-stream-consumer/src/main/java/net/safedata/reactive/spring/cloud/stream/CloudStreamConsumerApplication.java
+++ spring-cloud-stream-consumer/src/main/java/net/safedata/reactive/spring/cloud/stream/CloudStreamConsumerApplication.java
@@ -1,6 +1,8 @@
 package net.safedata.reactive.spring.cloud.stream;
 
 import net.safedata.reactive.spring.domain.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
@@ -15,11 +17,13 @@ import reactor.core.publisher.Flux;
 @EnableBinding(Sink.class) // 				--> more generic
 public class CloudStreamConsumerApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(CloudStreamConsumerApplication.class);
+
 	// will be called once, when the app starts, and it will continue to process data that arrives
 	@StreamListener
 	public void process(@Input(Sink.INPUT) final Flux<Order> ordersFlux) {
 		ordersFlux.map(order -> "[order]: " + order)
-				  .subscribe(System.out::println);
+				  .subscribe(order -> LOGGER.info("{}", order));
 	}
 
 	public static void main(String[] args) {

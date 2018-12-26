966ad72b0b88845875923b443d6dfe6340e0c4bf
Bogdan Solga <bogdan.solga@gmail.com>
2018-12-26 10:37:01 +0200

    [improve] Use an ApplicationRunner bean

--- reactive-webclient/webclient-usage/src/main/java/net/safedata/reactive/spring/webclient/ReactiveWebClientExample.java
+++ reactive-webclient/webclient-usage/src/main/java/net/safedata/reactive/spring/webclient/ReactiveWebClientExample.java
@@ -3,11 +3,12 @@ package net.safedata.reactive.spring.webclient;
 import net.safedata.reactive.spring.domain.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.Banner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -18,7 +19,7 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 @SpringBootApplication
-public class ReactiveWebClientExample implements CommandLineRunner {
+public class ReactiveWebClientExample {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ReactiveWebClientExample.class);
 
@@ -28,28 +29,30 @@ public class ReactiveWebClientExample implements CommandLineRunner {
         final SpringApplication application = new SpringApplication(ReactiveWebClientExample.class);
         application.setBannerMode(Banner.Mode.OFF);
         application.setWebApplicationType(WebApplicationType.NONE);
-        application.run(" -Dorg.slf4j.simpleLogger.defaultLogLevel=WARN");
+        application.run(args);
     }
 
-    @Override
-    public void run(String... args) {
-        System.out.println();
+    @Bean
+    ApplicationRunner applicationRunner() {
+        return args -> {
+            System.out.println();
 
-        final int productsCount = 10;
+            final int productsCount = 10;
 
-        final Instant start = Instant.now();
-        final List<Mono<Product>> products = IntStream.range(0, productsCount)
-                                                      .boxed()
-                                                      .map(id ->
-                                                              webClient.get()
-                                                                       .uri("/product/{id}", id)
-                                                                       .retrieve()
-                                                                       .bodyToMono(Product.class))
-                                                      .collect(Collectors.toList());
-        Mono.when(products)
-            .block();
+            final Instant start = Instant.now();
+            final List<Mono<Product>> products = IntStream.range(0, productsCount)
+                                                          .boxed()
+                                                          .map(id -> webClient.get()
+                                                                              .uri("/product/{id}", id)
+                                                                              .retrieve()
+                                                                              .bodyToMono(Product.class))
+                                                          .collect(Collectors.toList());
 
-        System.out.println();
-        LOGGER.info("Getting all the {} products took {} ms", productsCount, Duration.between(start, Instant.now()).toMillis());
+            Mono.when(products)
+                .block();
+
+            System.out.println();
+            LOGGER.info("Getting all the {} products took {} ms", productsCount, Duration.between(start, Instant.now()).toMillis());
+        };
     }
 }

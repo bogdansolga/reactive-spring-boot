2a6f9304356230cfcf6bcbcbf85f95be76ba0165
Bogdan Solga <bogdan.solga@gmail.com>
2023-05-25 17:41:20 +0300

    [dev] Added an async example

--- d01/d01s05-spring-webflux-overview/src/main/java/net/safedata/reactive/spring/controller/ProductController.java
+++ d01/d01s05-spring-webflux-overview/src/main/java/net/safedata/reactive/spring/controller/ProductController.java
@@ -2,10 +2,10 @@ package net.safedata.reactive.spring.controller;
 
 import net.safedata.reactive.spring.domain.Product;
 import org.reactivestreams.Publisher;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
@@ -38,11 +38,27 @@ public class ProductController {
     }
 
     @GetMapping(
-            value = "/stream",
-            produces = MediaType.TEXT_EVENT_STREAM_VALUE
+            value = "/stream"
+            //produces = MediaType.TEXT_EVENT_STREAM_VALUE
     )
     public Publisher<Product> infiniteStreamOfProducts() {
         return Flux.<Product>generate(sink -> sink.next(new Product(10, "The product for " + Instant.now(), 200)))
                    .delayElements(Duration.ofSeconds(1));
+    }
+
+    @GetMapping("/delay")
+    public Flux<String> delayed() {
+        return  Flux.fromArray("A stream with a lot of elements in it".split(" "))
+                    .delayElements(Duration.ofMillis(300));
+    }
+
+    @GetMapping("/deferred")
+    public DeferredResult<String> deferredResult() {
+        DeferredResult<String> deferredResult = new DeferredResult<>();
+        deferredResult.setResult("Response");
+        deferredResult.setErrorResult("An issue");
+        deferredResult.onTimeout(() -> deferredResult.setErrorResult("Has timed-out"));
+
+        return deferredResult;
     }
 }
